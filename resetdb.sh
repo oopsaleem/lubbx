3ff784e8ff16b876847b738c5eda52fc06cf4229
oopsaleem <robidy.ab@gmail.com>
2026-01-03 00:09:16 +0100

    resetdb dev script

---
+++ resetdb.sh
@@ -0,0 +1,15 @@
+docker compose down
+docker volume rm farm_db_gf
+docker volume rm farm_minio_data
+docker compose up -d
+
+rm -rf ./packages/database/prisma/migrations/
+rm -rf packages/database/prisma/zod
+
+rm -rf .next node_modules/.cache
+
+pnpm --filter database migrate
+
+pnpm --filter scripts seed:user
+
+pnpm dev
