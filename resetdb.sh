859e7bdf8c53d3b2c7f29776882b830e807ce7f6
oopsaleem <robidy.ab@gmail.com>
2026-01-30 21:16:04 +0100

    settings

--- resetdb.sh
+++ resetdb.sh
@@ -4,12 +4,16 @@ docker volume rm farm_minio_data
 docker compose up -d
 
 rm -rf ./packages/database/prisma/migrations/
-rm -rf packages/database/prisma/zod
+rm -rf ./packages/database/prisma/zod
 
-rm -rf .next node_modules/.cache
+rm -rf ./.turbo
+rm -rf ./apps/web/.next
+rm -rf ./apps/web/.turbo
+rm -rf ./apps/web/.content-collections/
 
 pnpm --filter database migrate:init init_db
 
 pnpm --filter scripts seed:user
+# pnpm --filter scripts seed:farm
 
 pnpm dev
--- tooling/scripts/src/seed-user.ts
+++ tooling/scripts/src/seed-user.ts
@@ -29,19 +29,139 @@ async function main() {
 		},
 		{
 			email: "user1@gmail.com",
-			name: "User No1",
+			name: "User No01",
 			isAdmin: false,
 			onboardingComplete: false,
 		},
 		{
 			email: "user2@gmail.com",
-			name: "User No2",
+			name: "User No02",
 			isAdmin: false,
 			onboardingComplete: false,
 		},
 		{
 			email: "user3@gmail.com",
-			name: "User No3",
+			name: "User No03",
+			isAdmin: false,
+			onboardingComplete: false,
+		},
+		{
+			email: "user4@gmail.com",
+			name: "User No04",
+			isAdmin: false,
+			onboardingComplete: false,
+		},
+		{
+			email: "user5@gmail.com",
+			name: "User No05",
+			isAdmin: false,
+			onboardingComplete: false,
+		},
+		{
+			email: "user6@gmail.com",
+			name: "User No06",
+			isAdmin: false,
+			onboardingComplete: false,
+		},
+		{
+			email: "user7@gmail.com",
+			name: "User No07",
+			isAdmin: false,
+			onboardingComplete: false,
+		},
+		{
+			email: "user8@gmail.com",
+			name: "User No08",
+			isAdmin: false,
+			onboardingComplete: false,
+		},
+		{
+			email: "user9@gmail.com",
+			name: "User No09",
+			isAdmin: false,
+			onboardingComplete: false,
+		},
+		{
+			email: "user10@gmail.com",
+			name: "User No10",
+			isAdmin: false,
+			onboardingComplete: false,
+		},
+		{
+			email: "user11@gmail.com",
+			name: "User No11",
+			isAdmin: false,
+			onboardingComplete: false,
+		},
+		{
+			email: "user12@gmail.com",
+			name: "User No12",
+			isAdmin: false,
+			onboardingComplete: false,
+		},
+		{
+			email: "user13@gmail.com",
+			name: "User No13",
+			isAdmin: false,
+			onboardingComplete: false,
+		},
+		{
+			email: "user14@gmail.com",
+			name: "User No14",
+			isAdmin: false,
+			onboardingComplete: false,
+		},
+		{
+			email: "user15@gmail.com",
+			name: "User No15",
+			isAdmin: false,
+			onboardingComplete: false,
+		},
+		{
+			email: "user16@gmail.com",
+			name: "User No16",
+			isAdmin: false,
+			onboardingComplete: false,
+		},
+		{
+			email: "user17@gmail.com",
+			name: "User No17",
+			isAdmin: false,
+			onboardingComplete: false,
+		},
+		{
+			email: "user18@gmail.com",
+			name: "User No18",
+			isAdmin: false,
+			onboardingComplete: false,
+		},
+		{
+			email: "user19@gmail.com",
+			name: "User No19",
+			isAdmin: false,
+			onboardingComplete: false,
+		},
+		{
+			email: "user20@gmail.com",
+			name: "User No20",
+			isAdmin: false,
+			onboardingComplete: false,
+		},
+		{
+			email: "user21@gmail.com",
+			name: "User No21",
+			isAdmin: false,
+			onboardingComplete: false,
+		},
+		{
+			email: "user22@gmail.com",
+			name: "User No22",
+			isAdmin: false,
+			onboardingComplete: false,
+		},
+		{
+			email: "user23@gmail.com",
+			name: "User No23",
 			isAdmin: false,
 			onboardingComplete: false,
 		},
@@ -80,10 +200,8 @@ async function main() {
 			accountId: adminUser.id,
 			hashedPassword,
 		});
-
-		logger.success("User created successfully!");
-		logger.info(`New user ${email} created! Password: ${adminPassword}`);
 	});
+	logger.success("Users created successfully!");
 }
 
 main();
